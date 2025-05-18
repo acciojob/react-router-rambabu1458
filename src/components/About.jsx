01f8a250a6eaa1999d2ccdb2f6876ccf571ec776
rambabu1458 <ramu.y95@gmail.com>
2025-05-18 11:34:35 +0530

    routes

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>This is a sample React Router program.</div>
+  )
+}
+
+export default About
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import Navigation from "./Navigation";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route exact path="/">
+          <Home/>
+          </Route>
+          <Route path="/about"> 
+          <About />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
